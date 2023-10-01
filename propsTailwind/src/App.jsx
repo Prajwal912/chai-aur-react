d04351b488981ddb841e66910b9d1c43f022269d
prajwal <prajwalshah912@gmail.com>
2023-10-02 00:08:48 +0530

    tailwind and props added with devUI card component

---
+++ propsTailwind/src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import Card from "./components/Card";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <h1 className="bg-green-100 text-black mb-4 rounded-lg">Tailwind</h1>
+
+      <Card btn='View' />
+      <Card btn='Show' />
+    </>
+  );
+}
+
+export default App;
