352a32f10534888e7a21f52b3ddefe2188cf481c
prajwal <prajwalshah912@gmail.com>
2023-11-17 22:19:35 +0530

    Basic Setup and design for router project

--- currencyConvertor/src/hooks/useCurrency.js
+++ currencyConvertor/src/hooks/useCurrency.js
@@ -1,16 +1,16 @@
 import { useEffect, useState } from "react";
 
-function useCurrency(currency) {
+function useCurrency(currencyKey) {
   const [data, setData] = useState({});
   useEffect(() => {
     fetch(
-      `https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/${currency}.json`
+      `https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/${currencyKey}.json`
     )
       .then((res) => res.json())
       .then((res) => {
-        setData(res[currency])
+        setData(res[currencyKey])
       });
-  }, [currency]);
+  }, [currencyKey]);
 
   return data;
 }
