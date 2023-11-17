d8d31b6c6b95ed2f2e0936bd2093fbe0352e2fe9
prajwal <prajwalshah912@gmail.com>
2023-11-17 23:16:05 +0530

    old way tohandle the routing

--- reactRouter/React-router/src/main.jsx
+++ reactRouter/React-router/src/main.jsx
@@ -6,17 +6,17 @@ import Layout from './Layout'
 import Home from './Components/Home/Home'
 import About from './Components/About/About'
 import Contact from './Components/Header/Contact/Contact'
-const router = createBrowserRouter([
-  {
-    path:'/', 
-    element:<Layout />,
-    children:[
-      {path: '', element:<Home />},
-      {path: 'about', element:<About />},
-      {path: 'contact', element:<Contact />},
-    ]
-  }
-])
+// const router = createBrowserRouter([
+//   {
+//     path:'/', 
+//     element:<Layout />,
+//     children:[
+//       {path: '', element:<Home />},
+//       {path: 'about', element:<About />},
+//       {path: 'contact', element:<Contact />},
+//     ]
+//   }
+// ])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
